e8b541f64fe625d25814331adb60fcceb8939a70
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2024-09-08 13:36:53 +0700

    Added code and exe files

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab0
+{
+    public partial class Form1 : Form
+    {
+        public double v, t, L;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            v = Convert.ToDouble(textBox1.Text);
+            t = Convert.ToDouble(textBox2.Text);
+            L = Convert.ToDouble(textBox3.Text);
+
+            MessageBox.Show("Скорость катера равна " + (L / t + v) + " м/с", "Результат", MessageBoxButtons.OK);
+        }
+    }
+}
